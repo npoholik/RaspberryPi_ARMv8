d192ad43512965a83e2839177fecf98c62e460fe
Nick P <nick.poholik@slu.edu>
2024-03-26 00:22:22 -0500

    Pi Estimate Working

--- Monte_Carlo.s
+++ Monte_Carlo.s
@@ -8,7 +8,12 @@
 //-------------------------------------------------------------------------------
 
 _start:
-    mov x0, #10
+    // mov x0, 0x86A0, lsl #0
+    // mov x0, 0x0001, lsl #0
+
+    movz x0, 0xFFFF, lsl #0
+    movk x0, 0x0001, lsl #16
+
     ldr x8, =count
 
     str x0, [x8, 0]
@@ -42,6 +47,9 @@ _start:
 
 //****************************************************************************
 
+mov x14, #0 // x14 = # of total points
+mov x15, #0 // x15 = # of points inside circle
+
 repeat:
     mov x8, #278 // Setup for Syscall 278 - getrandom
     ldr x0, =var // buffer address
@@ -58,18 +66,63 @@ repeat:
     ldr d1, [x0, 0]
     fdiv d0, d0, d1 // make random number be between -1 and 1
 
-    ldr x0, =string // Load format string for printf
+    fmul d3, d0, d0 // get x^2
+
+    mov x8, #278 // Setup for Syscall 278 - getrandom
+    ldr x0, =var // buffer address
+    mov x1, #8 // 8 bytes of randomness
+    mov x2, #0 // flags ?
+    svc #0
+
     ldr x8, =var // get address of variables
-    ldr x2, [x8, 0] // Paramter 2 - the floating point version of the number
-    mov x1, x10
-    bl printf // x0 - format string, x1 - first decimal, d0 - first floating point
+    ldr x9, [x8, 0] // load random number
+    scvtf d0, x9 // convert to double percinsion
+    str d0, [x8, 8] // store double percision
+
+    ldr x0, =divisor // Load divisor from ram
+    ldr d1, [x0, 0]
+    fdiv d0, d0, d1 // make random number be between -1 and 1
+
+    fmul d4, d0, d0 // get y^2
+
+    fadd d5, d3, d4 // get x^2 + y^2 
+
+    mov x12, #1 
+    scvtf d6, x12 // convert 1 to floating point 
+
+    add x14, x14, x12 //increment total num of points by 1
+
+    fcmp d5, d6
+    b.gt repeat
+
+    add x15, x15, x12 // increment points within circle by 1
+
+
+ //   ldr x0, =string // Load format string for printf
+ //   ldr x8, =var // get address of variables
+ //   ldr x2, [x8, 0] // Paramter 2 - the floating point version of the number
+ //   mov x1, x10
+ //   bl printf // x0 - format string, x1 - first decimal, d0 - first floating point
 
     ldr x8, =count // decrement the repeat counter
     ldr x10, [x8, 0]
     sub x10, x10, #1
     str x10, [x8, 0]
-    cbz x10, _exit
+    cbz x10, _calculate
     b repeat
+
+_calculate:
+    mov x1, #4 // move 4 into a reg
+    scvtf d0, x1 // convert 4 to floating point
+    scvtf d2, x15 // convert # of points in circle to floating point
+    scvtf d3, x14 // convert # of points outside circle to floating point
+    fmul d0, d0, d2 // do 4 * # points in circle
+    fdiv d0, d0, d3 // final calculations of pi
+
+    ldr x0, =string
+    ldr x8, =var
+    str d0, [x8, 0]
+    bl printf
 
 // Exit code:
 _exit:
@@ -80,9 +133,12 @@ _exit:
 //----------------------------------------------------------------------------
 
 .data
-string:
-.asciz "Num: %d: %lld %lf\n"
+//string:
+//.asciz "Num: %d: %lld %lf\n"
 //.bss // variable
+
+string:
+.asciz "Pi: %lf\n"
 
 var:
         .zero 8
